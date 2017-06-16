0edfa22c6bc429da05955d9c421221f6e4b6e7ac
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-06-16 07:40:32 -0400

    Added http get implementation

--- src/main/java/io/zipcoder/crudapp/entity/Person.java
+++ src/main/java/io/zipcoder/crudapp/entity/Person.java
@@ -15,8 +15,8 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private String first_name;
-    private String last_name;
+
+    private String name;
 
     public Integer getId() {
         return id;
@@ -26,22 +26,13 @@ public class Person {
         this.id = id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getName() {
+        return name;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
 
     Person(){
     }
